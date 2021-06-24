7ff9552e3002b947affb7992563b49cad32f9bf0
Pommepoirechocolat <85391741+pommepoirechocolat@users.noreply.github.com>
2021-06-24 20:44:46 +0200

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,11 @@
+@echo off
+python -m pip
+timeout /t 1
+pip install discord
+timeout /t 1
+pip install asyncio
+timeout /t 1
+pip install discord.ext
+timeout /t 1
+tous les module on etais installer veuiller fermer le programe ou il se fermera dans 10 seconde
+timeout /t 10
